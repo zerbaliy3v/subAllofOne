be6998e7c057fc874d8582f139dbecfe50caf977
Zerbaliy3v <128548437+zerbaliy3v@users.noreply.github.com>
2023-07-19 16:08:27 +0000

    Update SubAllofOne.sh

--- SubAllofOne.sh
+++ SubAllofOne.sh
@@ -1,10 +1,8 @@
 #/bin/bash
-domain =$1
-
-subfinder  -d $domain -o s1 && amass enum  -passive -norecursive -noalts -d $domain -o s2 && assetfinder $domain > s3  && curl -s "https://crt.sh/?q=%25.$domain&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u > s4;
+subfinder  -d $1 -o s1 && amass enum  -passive -norecursive -noalts -d $1 -o s2 && assetfinder $1 > s3  && curl -s "https://crt.sh/?q=%25.$1&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u > s4;
 
 #brute force
-findomain -t $domain  -w /usr/local/danielmiessler/SecLists/Discovery/DNS/subdomains-top1million-110000.txt --unique-output s5;
+findomain -t $1  -w /usr/local/danielmiessler/SecLists/Discovery/DNS/subdomains-top1million-110000.txt --unique-output s5;
 
 cat s1 | anew subdomain && cat s2 | anew subdomain && cat s3 | anew subdomain && cat s4 | anew subdomain && rm -rf s1 s2 s3 s4 s5;
 
